f001c8e9f39b482fd20c4a99e98561e19e01cd44
Neltharion <NtNeltharion@gmail.com>
2026-05-28 14:44:22 +0300

    Build scripts

---
+++ winbuild.sh
@@ -0,0 +1,1 @@
+RUSTFLAGS=-L. cargo run --target x86_64-pc-windows-gnullvm --example deadlock
---
+++ winrun.sh
@@ -0,0 +1,1 @@
+BOX64_LD_LIBRARY_PATH=~/box64/x64lib ~/box64/build/box64 ~/wine/bin/wine target/x86_64-pc-windows-gnullvm/debug/examples/args.exe
