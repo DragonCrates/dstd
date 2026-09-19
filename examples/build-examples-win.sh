681adec2bc1b3d93f93a4da5cfd01ce463950086
Neltharion <NtNeltharion@gmail.com>
2026-09-19 19:15:41 +0300

    Added windows build scripts

---
+++ examples/build-examples-win.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+cd examples
+for i in *.rs; do
+    cargo build --example ${i/.rs/} --target x86_64-pc-windows-gnullvm
+done
---
+++ wincheck.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cargo check --target x86_64-pc-windows-gnullvm
