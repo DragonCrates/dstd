8ad4a08817fc347f4a3fd933f1261c592229e5b5
Neltharion <NtNeltharion@gmail.com>
2026-09-20 13:06:59 +0300

    Changed scripts from bash to sh

--- examples/build-examples-win.sh
+++ examples/build-examples-win.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 cd examples
 for i in *.rs; do
-    cargo build --example ${i/.rs/} --target x86_64-pc-windows-gnullvm
+    cargo build --example ${i%.rs} --target x86_64-pc-windows-gnullvm
 done
