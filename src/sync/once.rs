79416e24bd8449bece533cbaa68af1875ef6b297
Neltharion <NtNeltharion@gmail.com>
2026-09-26 18:19:32 +0300

    Better sync::Once

--- src/sync/once.rs
+++ src/sync/once.rs
@@ -1,58 +1,103 @@
-use core::sync::atomic::AtomicBool;
-use core::sync::atomic::Ordering::{Relaxed, Acquire, Release};
+use core::sync::atomic::Ordering::*;
 
-use super::RawMutex;
+use super::sys::*;
 
-// TODO: make it use only one field
 /// A low-level synchronization primitive for one-time global execution
 pub struct Once {
-    inited: AtomicBool,
-    futex: RawMutex,
+    state: MiniFutex
 }
+
+const UNINIT: MiniPrimitive = 0;
+const LOCKED: MiniPrimitive = 1;
+const CONTENDED: MiniPrimitive = 2;
+const COMPLETE: MiniPrimitive = 3;
 
 impl Once {
     /// Creates a new Once value
     pub const fn new() -> Once {
         Once {
-            inited: AtomicBool::new(false),
-            futex: RawMutex::new(),
+            state: MiniFutex::new(UNINIT)
         }
     }
 
     pub(crate) const fn new_complete() -> Once {
         Once {
-            inited: AtomicBool::new(true),
-            futex: RawMutex::new(),
+            state: MiniFutex::new(COMPLETE)
         }
     }
 
     /// Performs the initialization only once
     pub fn call_once<F: FnOnce()>(&self, f: F) {
-        if !self.inited.load(Acquire) {
-            self.futex.lock();
-            if !self.inited.load(Relaxed) {
-                f();
-                self.inited.store(true, Release);
+        let state = self.state.load(Acquire);
+        if state == COMPLETE {
+            return;
+        } else {
+            self.call_once_slow(state, f);
+        }
+    }
+
+    #[cold]
+    fn call_once_slow<F: FnOnce()>(&self, mut state: MiniPrimitive, f: F) {
+        loop {
+            if state == UNINIT {
+                match self.state.compare_exchange(UNINIT, LOCKED, Acquire, Acquire) {
+                    Ok(_) => {
+                        // Lock acquired, perform init
+                        f();
+                        // Done, now unlock
+                        let old = self.state.swap(COMPLETE, Release);
+                        if old == CONTENDED {
+                            futex_wake_all(&self.state);
+                        }
+                        return;
+                    }
+                    Err(actual) => state = actual
+                }
+            } else if state == LOCKED {
+                // It is locked, register contention
+                match self.state.compare_exchange_weak(LOCKED, CONTENDED, Relaxed, Acquire) {
+                    Ok(_) | Err(CONTENDED) => {
+                        // Now, wait
+                        futex_wait(&self.state, CONTENDED);
+                        // Woke up, reload the state
+                        state = self.state.load(Acquire);
+                    }
+                    Err(actual) => state = actual
+                }
+            } else if state == CONTENDED {
+                // Already contended, just wait
+                futex_wait(&self.state, CONTENDED);
+                state = self.state.load(Acquire);
+            } else if state == COMPLETE {
+                // We are done
+                return;
+            } else {
+                #[cfg(debug_assertions)]
+                panic!("unreachable Once state: {state}");
             }
-            self.futex.unlock();
         }
     }
 
     pub(crate) fn call_once_mut<F: FnOnce()>(&mut self, f: F) {
-        let inited = self.inited.get_mut();
-        if !*inited {
+        let state = self.state.get_mut();
+        if *state == UNINIT {
             f();
-            *inited = true;
+            *state = COMPLETE;
+        } else if *state == COMPLETE {
+            // Do nothing
+        } else {
+            #[cfg(debug_assertions)]
+            panic!("unreachable Once state: {state}");
         }
     }
 
     /// Returns `true` if initializarion is completed
     pub fn is_completed(&self) -> bool {
-        self.inited.load(Acquire)
+        self.state.load(Acquire) == COMPLETE
     }
 
     pub(crate) fn is_completed_mut(&mut self) -> bool {
-        *self.inited.get_mut()
+        *self.state.get_mut() == COMPLETE
     }
 }
 
